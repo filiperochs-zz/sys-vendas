2adba63158b48267c946df714acedc872b3bb71d
filiperochs <filipesilva.2001ccv@gmail.com>
2019-10-10 08:31:31 -0300

    complementações no sistema

--- src/br/com/trab1pc2/testes/Main.java
+++ src/br/com/trab1pc2/testes/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	private static Sistema sis = Sistema.getInstance();
 
 	public static void main(String[] args) {
-
+		init();
 		menuAdministrador();
 
 	}
@@ -67,22 +67,16 @@ public class Main {
 				break;
 
 			case 2:
-				Produto[] produtos = sis.listarProdutos();
-				
-				System.out.printf("\n<--> Produtos: <-->\n");
-				for (int i = 0; i < produtos.length; i++) {
-					System.out.printf("ID: %d\nNome: %s\nDescrição: %s\nPreço: R$%.2f\nQuantidade em estoque: %d\n\n", produtos[i].getId(), produtos[i].getNome(), produtos[i].getDescricao(), produtos[i].getPreco(), produtos[i].getQtdEstoque());
-				}
-				System.out.printf("\n<--> -------------- <-->\n");
+				listarProdutos();
 				
 				break;
 
 			case 3:
 				while(opcao2 != 0) {
 					System.out.printf("\n<---> Menu de Criação de Pedidos <--->\n");
-					System.out.printf("1. Criar item de produtos.\n");
-					System.out.printf("2. Criar Pedido.\n");
-					System.out.printf("0. Voltar ao Menu Principal.\n");
+					System.out.printf("1. Adicionar Itens ao Pedido.\n");
+					System.out.printf("2. Finalizar Pedido.\n");
+					System.out.printf("0. Cancelar e Voltar.\n");
 					System.out.printf(": ");
 					opcao2 = inN.nextInt();
 					System.out.printf("\n");
@@ -91,13 +85,13 @@ public class Main {
 					case 0:
 						break;
 					case 1:
-						Produto produto = lerProduto();
+						int id, qtd;
 						
-						if (sis.inserirProduto(produto)) {
-							System.out.printf("Produto inserido com sucesso.\n");
-						} else {
-							System.out.println("Falha ao inserir produto.\n");
-						}
+						listarProdutos();
+						
+						System.out.println(lerItem());
+						
+						
 						break;
 					default:
 						System.out.printf("Opção Inválida!\n");
@@ -143,6 +137,51 @@ public class Main {
 		}
 		
 		return produto;
+	}
+	
+	private static Item lerItem() { 
+		
+		System.out.printf("\nID do Produto: ");
+		int id = inN.nextInt();
+		System.out.printf("\nQuantidade: ");
+		int qtd = inN.nextInt();
+		
+		Item item = sis.criarItem(id, qtd);
+		
+		if (item == null) {
+			System.out.printf("Dados incorretos!\n");
+			item = lerItem();
+		} else {
+			System.out.printf("Inserindo dados...\n");
+		}
+		
+		return item;
+	}
+	
+	private static void listarProdutos() {
+		Produto[] produtos = sis.listarProdutos();
+		
+		System.out.printf("\n<--> Lista de Produtos: <-->\n");
+		for (int i = 0; i < produtos.length; i++) {
+			System.out.printf("ID: %d\nNome: %s\nDescrição: %s\nPreço: R$%.2f\nQuantidade em estoque: %d\n\n", produtos[i].getId(), produtos[i].getNome(), produtos[i].getDescricao(), produtos[i].getPreco(), produtos[i].getQtdEstoque());
+		}
+		System.out.printf("\n<--> -------------- <-->\n");
+	}
+	
+	private static void init() {
+		Produto produto = Produto.getInstance("Jaca", "Fruta", 12.99, 50);
+		sis.inserirProduto(produto);
+		produto = Produto.getInstance("Uva", "Fruta", 10.99, 10);
+		sis.inserirProduto(produto);
+		produto = Produto.getInstance("Melão", "Fruta", 5.99, 40);
+		sis.inserirProduto(produto);
+		produto = Produto.getInstance("Melancia", "Fruta", 1.99, 5);
+		sis.inserirProduto(produto);
+		produto = Produto.getInstance("Maracujá", "Fruta", 16.99, 45);
+		sis.inserirProduto(produto);
+		sis.excluirProduto(3);
+		//sis.excluirProduto(3);
+		//sis.excluirProduto(5);
 	}
 
 }
